95ce105f510d036f84ddc55430b6d44087bbc22a
Abdul <abdulrhmn4137@gmail.com>
2025-04-04 22:29:47 +0530

    first commit

---
+++ backend_learning/app.js
@@ -0,0 +1,9 @@
+const add = require("./math");
+const multiply = require("./math");
+// const minus = require("./math");
+
+console.log(add(5, 10));  // Output: 15
+console.log(multiply(10,245));
+// console.log(minus(23, 84))
+
+console.log("Hello, Node.js!");
---
+++ backend_learning/math.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+    return a + b;
+}
+function multiply(a,b){
+    return a*b;
+}
+// function minus(a,b){
+//     return a-b;
+
+// }
+
+module.exports = add;  // Export function
+module.exports = multiply; // Export Mul function
+// module.exports = minus;
