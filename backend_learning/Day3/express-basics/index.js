a0ec4b1ac06c2fbc50f1b312dcad5279d6ff76b6
Abdul <abdulrhmn4137@gmail.com>
2025-04-06 19:46:51 +0530

    practiced routes in express

--- backend_learning/Day3/express-basics/index.js
+++ backend_learning/Day3/express-basics/index.js
@@ -12,6 +12,25 @@ app.get('/hello', (req, res) => {
   res.send('Hello, this is a GET route!');
 });
 
+//Home route
+
+app.get('/home' , (req , res) => {
+    res.send('Welcome to home Page');
+})
+//Contact Us Route 
+
+app.get('/contactus' , (req , res)=>{
+    const data = {name : 'abdul' , email : 'abdulrhmn4137@gmail'};
+    res.json(data);
+});
+
+
+//about us route
+
+app.get('/about' , (req , res)=>{
+    res.send('Welcome to About Us Page')
+})
+
 // Start server
 app.listen(3000, () => {
   console.log('Server is running at http://localhost:3000');
